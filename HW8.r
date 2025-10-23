8e4a97d94fc651db3eb5221e24a702765087433a
Evan Peepo <peepo@wisc.edu>
2025-10-23 14:51:04 -0500

    remote EDI connection

--- HW8.r
+++ HW8.r
@@ -23,3 +23,15 @@ library(EDIutils)
 ###################
 ### OBJECTIVE 1 ###
 ###################
+
+package_id <- 'knb-lter-sev.1.18'
+
+package_entities <- list_data_entities(package_id)
+package_entities
+
+data_entity <- read_data_entity(
+  packageId = package_id,
+  entityId = "65056476604ff22cb44dcc3d3bebfd2a"
+)
+
+sev_2020_2023 <- read_csv(data_entity)
