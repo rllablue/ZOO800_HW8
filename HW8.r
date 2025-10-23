a15c0d080c7700c7d32ddebf789724ebd68ac6a0
Rebekkah LaBlue <lablue.rebekkah@gmail.com>
2025-10-23 14:47:48 -0500

    Create HW8 base files.

---
+++ HW8.r
@@ -0,0 +1,25 @@
+###################
+### ZOO800: HW8 ###
+###################
+
+# Authors: Evan Peepo & Rebekkah LaBlue
+# Project: Online data repositories and exploratory data analysis
+# Due: October 27, 2025
+
+##############
+### SET UP ###
+#############
+
+library(tidyr)
+library(dplyr)
+library(magrittr)
+library(readr)
+library(stringr)
+library(lubridate)
+library(ggplot2)
+library(EDIutils)
+
+
+###################
+### OBJECTIVE 1 ###
+###################
